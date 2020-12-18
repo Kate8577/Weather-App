1d7860a4acad5ab7a9e937a08c7d823e3535777c
Kate8577 <katepodgornaya@gmail.com>
2020-12-18 07:32:58 -0800

    Added showMessage function

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ if (minutes < 10) {
   minutes = `0${minutes}`;
 }
 
-dateElement.innerHTML = `${day} ${hours}:${minutes}`;
+dateElement.innerHTML = `Last updated: ${day} ${hours}:${minutes}`;
 
 function searchCity(event) {
   event.preventDefault();
@@ -29,6 +29,8 @@ function searchCity(event) {
   cityElement.innerHTML = `${citySearch.value}`;
   search(citySearch.value);
 }
+search("San Diego");
+
 function search(city) {
   let apiKey = "72a6f55e4b65680441a701dfe7a8721f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
@@ -40,6 +42,7 @@ form.addEventListener("submit", searchCity);
 
 function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
+  
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = `${temperature}°F`;
 
@@ -48,4 +51,10 @@ function showTemperature(response) {
   descriptionElement.innerHTML = `${weatherDescription}`;
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+}
+function showMessage(){
+  let messageElement = document.querySelector("#message");
+if (temperature > 50) {
+  messageElement.innerHTML = "Good day";
+}
 }
